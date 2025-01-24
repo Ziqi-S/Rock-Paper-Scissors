a98507790a3879bf795f81affd15d2e8a8ff48a3
Ziqi-S <lazyair@MacBookPro.fritz.box>
2025-01-24 12:06:44 +0100

    reformating

--- scripts/rock-paper-scissors.js
+++ scripts/rock-paper-scissors.js
@@ -1,52 +1,52 @@
 let score = localStorage.getItem('score') ? JSON.parse(localStorage.getItem('score')) : {
-    wins: 0,
-    losses: 0,
-    ties: 0
+  wins: 0,
+  losses: 0,
+  ties: 0
 };
 
 updateScoreElement();//show up on the website
 
 function pickComputerMove(){
-    const randomNumber = Math.random();
-    let computerMove = '';
+  const randomNumber = Math.random();
+  let computerMove = '';
 
-    if(randomNumber >= 0 && randomNumber <1/3){
-      computerMove = 'Rock';
-    }else if(randomNumber >= 1/3 && randomNumber <2/3){
-      computerMove = 'Paper';
-    }else{
-      computerMove = 'Scissors';
-    }
-    return computerMove;
+  if(randomNumber >= 0 && randomNumber <1/3){
+    computerMove = 'Rock';
+  }else if(randomNumber >= 1/3 && randomNumber <2/3){
+    computerMove = 'Paper';
+  }else{
+    computerMove = 'Scissors';
+  }
+  return computerMove;
 }
 
 function playGame(choice){
-    let result = '';
-    const computerMove = pickComputerMove();
-    
-    if(computerMove === 'Rock' && choice === 'Scissors' || computerMove === 'Scissors' && choice === 'Paper' || computerMove === 'Paper' && choice === 'Rock'){
-      result = 'You lose!';
-      score.losses++;
-    }else if(computerMove === choice){
-      result = 'Tie!';
-      score.ties++;
-    }else{
-      result = 'You win!';
-      score.wins++;
-    }
-
-    localStorage.setItem('score', JSON.stringify(score));//localStorage can only handle Strings
-
-    document.querySelector('.js-result').innerHTML = result;
-    document.querySelector('.js-move').innerHTML = 
-        `You <img src="images/${choice.toLowerCase()}-emoji.png" class="move-icon">
-        <img src="images/${computerMove.toLowerCase()}-emoji.png" class="move-icon"> Computer`;
+  let result = '';
+  const computerMove = pickComputerMove();
   
-    updateScoreElement();//update the website showup
+  if(computerMove === 'Rock' && choice === 'Scissors' || computerMove === 'Scissors' && choice === 'Paper' || computerMove === 'Paper' && choice === 'Rock'){
+    result = 'You lose!';
+    score.losses++;
+  }else if(computerMove === choice){
+    result = 'Tie!';
+    score.ties++;
+  }else{
+    result = 'You win!';
+    score.wins++;
+  }
+
+  localStorage.setItem('score', JSON.stringify(score));//localStorage can only handle Strings
+
+  document.querySelector('.js-result').innerHTML = result;
+  document.querySelector('.js-move').innerHTML = 
+    `You <img src="images/${choice.toLowerCase()}-emoji.png" class="move-icon">
+    <img src="images/${computerMove.toLowerCase()}-emoji.png" class="move-icon"> Computer`;
+
+  updateScoreElement();//update the website showup
 }
 
 function updateScoreElement(){
-    document.querySelector('.js-score').innerHTML = 'Wins: ' + score.wins + ', Losses: ' + score.losses + ', Ties: ' + score.ties;
+  document.querySelector('.js-score').innerHTML = 'Wins: ' + score.wins + ', Losses: ' + score.losses + ', Ties: ' + score.ties;
 }
 
 //---------------------------Auto Play---------------------------//
@@ -55,10 +55,10 @@ let intervalId;
 
 function autoPlay(){
   if(isAutoPlaying){
-        clearInterval(intervalId);
-        isAutoPlaying = false;
-        document.querySelector('.autoplay-button').innerText = 'Auto Play';
-        return;
+    clearInterval(intervalId);
+    isAutoPlaying = false;
+    document.querySelector('.autoplay-button').innerText = 'Auto Play';
+    return;
   }
   intervalId = setInterval(() => {
     const playerMove = pickComputerMove();
@@ -101,12 +101,13 @@ document.querySelector('.autoplay-button')
   })
 
 //---------------------------Play game with keydown---------------------------//
-document.body.addEventListener('keydown', (event) => {
-  if(event.key === 'r'){
-    playGame('Rock');
-  }else if(event.key === 'p'){
-    playGame('Paper');
-  }else if(event.key === 's'){
-    playGame('Scissors')
-  }
-});
+document.body
+  .addEventListener('keydown', (event) => {
+    if(event.key === 'r'){
+      playGame('Rock');
+    }else if(event.key === 'p'){
+      playGame('Paper');
+    }else if(event.key === 's'){
+      playGame('Scissors')
+    }
+  });
